d8138b2df706c9ed9196b47381873715288ac703
Geta0745 <82598256+Geta0745@users.noreply.github.com>
2022-10-31 10:02:05 +0700

    gsrdg

--- saveform.php
+++ saveform.php
@@ -1,6 +1,6 @@
 <?php
 require 'conn.php';
-$sql_update="INSERT INTO member(username,password,gender,education,email,hobby) VALUES ('$_GET[username]','$_GET[password]' ,'$_GET[gender]' ,'$_GET[education]','$_GET[email]','$_GET[hobby]')";
+$sql_update="INSERT INTO customer(username,password,gender,education,email,hobby) VALUES ('$_GET[username]','$_GET[password]' ,'$_GET[gender]' ,'$_GET[education]','$_GET[email]','$_GET[hobby]')";
 
 $result= $conn->query($sql_update);
 
@@ -9,7 +9,7 @@ if(!$result) {
 } else {
 
 echo "Success <br>";
-header("refresh: 1; url=http://localhost/final_test/final_test/showform01.php");
+header("refresh: 1; url=http://localhost/final_test/final_test/showform01.php?username=$_GET[username]&password=$_GET[password]&gender=$_GET[gender]&education=$_GET[education]&email=$_GET[email]&hobby=$_GET[hobby]");
 }
 
 ?>
---
+++ showform01.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <img src="logo.png" style="width: 40px;">
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h2>Username : <?php echo $_GET['username'] ?></h2><br>
+        <h2>password : <?php echo $_GET['password'] ?></h2><br>
+        <h2>Gender : <?php echo $_GET['gender'] ?></h2><br>
+        <h2>education : <?php echo $_GET['education'] ?></h2><br>
+        <h2>email : <?php echo $_GET['email'] ?></h2><br>
+        <h2>hobby : <?php echo $_GET['hobby'] ?></h2><br>
+
+        <a class="btn btn-primary" href="index.html">ตกลก</a>
+    </div>
+
+
+
+    <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2020 Copyright:
+            <a class="text-dark" href="#">chatty.CTM</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+
+</body>
+
+</html>
